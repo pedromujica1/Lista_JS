678814b6750d443eb3dbd05b31f0d82b66e2ff10
pedromujica1 <miottopedro@gmail.com>
2023-04-03 20:15:35 -0300

    Arquivos feitos em sala dia 03 de Abril

---
+++ lista.js
@@ -0,0 +1,26 @@
+const nameList = ["Aline","Otavio","Camila","Nareba","Jean","Julio","Julio Rodalo","Rany","Ti"]
+
+const listEl = document.getElementById("list");
+const searchField = document.getElementById("searchField");
+searchField.addEventListener("input",inputHandler);
+
+this.fillList()
+
+function fillList(list=nameList){
+    listEl.innerHTML="";
+    for (let i=0;i<list.length;i++){
+        let listItems = document.createElement("li");
+        listItems.innerHTML = list[i];
+        listEl.appendChild(listItems);
+    }
+}
+function inputHandler(){
+    const filteredlist = nameList.filter(el => {
+        const listItems = el.toLowerCase(); 
+        const searchWord = searchField.value.toLowerCase();
+        return listItems.includes(searchWord)
+    });
+    fillList(filteredlist);
+}
+
+
